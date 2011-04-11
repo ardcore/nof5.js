a363ea37ffb0995452d161cb1059c8d6fe617ab4
Szymon Piłkowski <szymon.pilkowski@gmail.com>
2011-04-11 15:53:21 +0000

    fixed formatting

--- nof5client.js
+++ nof5client.js
@@ -17,15 +17,15 @@ function loadScript(url, callback) {
 };
 
 function reCss() {
-    var i, a, s;
-    a = document.getElementsByTagName('link');
-    for (i = 0; i < a.length; i++) {
-        s = a[i];
-        if (s.rel.toLowerCase().indexOf('stylesheet') >= 0 && s.href) {
-            var h = s.href.replace(/(&|\?)forceReload=\d+/, '');
-            s.href = h + (h.indexOf('?') >= 0 ? '&' : '?') + 'forceReload=' + (new Date().valueOf())
-        }
+  var i, a, s;
+  a = document.getElementsByTagName('link');
+  for (i = 0; i < a.length; i++) {
+    s = a[i];
+    if (s.rel.toLowerCase().indexOf('stylesheet') >= 0 && s.href) {
+      var h = s.href.replace(/(&|\?)forceReload=\d+/, '');
+      s.href = h + (h.indexOf('?') >= 0 ? '&' : '?') + 'forceReload=' + (new Date().valueOf())
     }
+  }
 };
 
 loadScript("http://cdn.socket.io/stable/socket.io.js", function() {
